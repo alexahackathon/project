f9c70885db02f98bce936113286315287205385e
Chris Chang <chrischang43@gmail.com>
2016-08-06 15:26:54 -0700

    creating the landing page and styles

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var express = require("express"),
     morgan = require("morgan"),
     bodyParser = require("body-parser");
 
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 app.use(morgan("tiny"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
 app.get("/", function(req,res){
-  res.render("index");
+  res.render("home");
 });
 
 app.get("*", function(req,res){
-  res.render("404");
+  res.status("404");
 });
 
 app.listen(3000, function(){
--- migrations/20160806142912_contacts_table.js
+++ migrations/20160806142912_contacts_table.js
@@ -1,4 +1,3 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('contacts', function(table){
     table.increments();
